06037bc9a34e40c11d1ca5fa43ba23591aa2f7e6
Jukunye <jukunyes@gmail.com>
2026-09-24 22:23:50 +0300

    fix(config): :bug: use explicit default kwarg in secret_key Field

--- core/config.py
+++ core/config.py
@@ -1,10 +1,6 @@
-import os
-from typing import Literal
 from pydantic import Field
-from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-BASE_DIR = Path(__file__).resolve().parent
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
@@ -17,10 +13,11 @@ class Settings(BaseSettings):
     app_name: str = "Shugli API"
 
     # --- Security ---
-    secret_key: str = BASE_DIR / Field(..., min_length=32)
+    secret_key: str = Field(default=..., min_length=32)
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
     refresh_token_expire_days: int = 7
+
 
 
 settings = Settings()
